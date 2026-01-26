f0c7a5f3574039d496e9f1e4e71990982d7a8450
Joan Brotons <joanbv2006@gmail.com>
2026-01-26 18:04:37 +0100

    ya esta todo hecho

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import express from 'express';
 
+import suppliersRouter from './routes/suppliers.routes.js';
 import productsRouter from './routes/products.routes.js';
 import notFound from './middlewares/not-found.js';
 import errorHandler from './middlewares/error-handler.js';
@@ -10,10 +11,13 @@ import categoriesRouter from './routes/categories.routes.js';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './swagger.js';
 
-
-
-
 const app = express();
+
+app.use('/api-docs', (_req, res, next) => {
+  res.removeHeader('Content-Security-Policy');
+  next();
+});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(helmet());
 app.use(cors());
@@ -24,16 +28,9 @@ app.use('/api/v1/categories', categoriesRouter);
 app.get('/health', (_req, res) => {
   res.status(200).json({ status: 'ok' });
 });
-app.use('/api-docs', (_req, res, next) => {
-  res.removeHeader('Content-Security-Policy');
-  next();
-});
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));  
 
 app.use('/api/v1/products', productsRouter);
-
-
-
+app.use('/api/v1/suppliers', suppliersRouter);
 
 app.use(notFound);
 app.use(errorHandler);
